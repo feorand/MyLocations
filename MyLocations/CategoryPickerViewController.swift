553f7a125d86bcb7963154e8b5224d1f6208f630
Pavel Prokofyev <feorand@gmail.com>
2017-09-23 15:04:33 +0500

    add checkmark placement when user selects a category in CategoryPickerVC

--- MyLocations/CategoryPickerViewController.swift
+++ MyLocations/CategoryPickerViewController.swift
@@ -22,13 +22,27 @@ class CategoryPickerViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "locationCategory", for: indexPath)
         cell.textLabel?.text = LocationCategories.categories[indexPath.row]
-        cell.accessoryType = indexPath.row == categoryId ? .checkmark : .none
+        if categoryId == indexPath.row {
+            UpdateAccessory(ofCell: cell)
+        }
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        UpdateAccessory(ofCellWithId: categoryId)
+        UpdateAccessory(ofCellWithId: indexPath.row)
         delegate?.didChooseCategory(withId: indexPath.row)
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    private func UpdateAccessory(ofCellWithId id:Int) {
+        let indexPath = IndexPath(row: id, section: 0)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "locationCategory", for: indexPath)
+        UpdateAccessory(ofCell: cell)
+    }
+    
+    private func UpdateAccessory(ofCell cell: UITableViewCell) {
+        cell.accessoryType = cell.accessoryType == .checkmark ? .none : .checkmark
     }
 }
